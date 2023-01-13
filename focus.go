80ddbe66d09ba174209823ef21951b6e8a59752f
Simon Tucker <brotherlogic@gmail.com>
2023-01-13 14:53:48 -0800

    First pass

---
+++ focus.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/brotherlogic/goserver"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	dstore_client "github.com/brotherlogic/dstore/client"
+	github_client "github.com/brotherlogic/githubcard/client"
+
+	pbg "github.com/brotherlogic/goserver/proto"
+	"github.com/brotherlogic/goserver/utils"
+	pb "github.com/brotherlogic/tasklist/proto"
+)
+
+// Server main server type
+type Server struct {
+	*goserver.GoServer
+	dclient  *dstore_client.DStoreClient
+	ghclient *github_client.GHClient
+}
+
+// Init builds the server
+func Init() *Server {
+	s := &Server{
+		GoServer: &goserver.GoServer{},
+	}
+	s.dclient = &dstore_client.DStoreClient{Gs: s.GoServer}
+	s.ghclient = &github_client.GHClient{Gs: s.GoServer}
+	return s
+}
+
+// DoRegister does RPC registration
+func (s *Server) DoRegister(server *grpc.Server) {
+
+}
+
+// ReportHealth alerts if we're not healthy
+func (s *Server) ReportHealth() bool {
+	return true
+}
+
+// Shutdown the server
+func (s *Server) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+// Mote promotes/demotes this server
+func (s *Server) Mote(ctx context.Context, master bool) error {
+	return nil
+}
+
+// GetState gets the state of the server
+func (s *Server) GetState() []*pbg.State {
+	return []*pbg.State{
+		&pbg.State{Key: "magic", Value: int64(12345)},
+	}
+}
+
+func main() {
+	var quiet = flag.Bool("quiet", false, "Show all output")
+	flag.Parse()
+
+	//Turn off logging
+	if *quiet {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+	server := Init()
+	server.PrepServer("focus")
+	server.Register = server
+
+	err := server.RegisterServerV2(false)
+	if err != nil {
+		return
+	}
+
+	ctx, cancel := utils.ManualContext("tasklist-init", time.Minute)
+	server.readConfig(ctx)
+	cancel()
+
+	fmt.Printf("%v", server.Serve())
+}
---
+++ focusapi.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	dspb "github.com/brotherlogic/dstore/proto"
+	pb "github.com/brotherlogic/tasklist/proto"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	CONFIG_KEY = "github.com/brotherlogic/tasklist/config"
+
+	lists = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "tasklist_lists",
+	})
+)
+
+func (s *Server) metrics(config *pb.Config) {
+	lists.Set(float64(len(config.GetLists())))
+}
+
+func (s *Server) readConfig(ctx context.Context) (*pb.Config, error) {
+	data, err := s.dclient.Read(ctx, &dspb.ReadRequest{Key: CONFIG_KEY})
+	if err != nil {
+		if status.Code(err) != codes.InvalidArgument {
+			return nil, err
+		}
+		data = &dspb.ReadResponse{}
+	}
+
+	config := &pb.Config{}
+	err = proto.Unmarshal(data.GetValue().GetValue(), config)
+
+	if err == nil {
+		s.metrics(config)
+	}
+
+	return config, err
+}
+
+func (s *Server) saveConfig(ctx context.Context, config *pb.Config) error {
+	data, _ := proto.Marshal(config)
+	_, err := s.dclient.Write(ctx, &dspb.WriteRequest{Key: CONFIG_KEY, Value: &anypb.Any{Value: data}})
+	return err
+}
+
+func (s *Server) GetTaskLists(ctx context.Context, req *pb.GetTaskListsRequest) (*pb.GetTaskListsResponse, error) {
+	config, err := s.readConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetTaskListsResponse{Lists: config.GetLists()}, nil
+}
+
+func (s *Server) AddTaskList(ctx context.Context, req *pb.AddTaskListRequest) (*pb.AddTaskListResponse, error) {
+	config, err := s.readConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	found := false
+	for _, list := range config.GetLists() {
+		if list.GetName() == req.GetAdd().GetName() {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		return nil, status.Errorf(codes.AlreadyExists, "%v already exists", req.GetAdd().GetName())
+	}
+
+	config.Lists = append(config.Lists, req.GetAdd())
+
+	err = s.processTaskLists(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.AddTaskListResponse{}, s.saveConfig(ctx, config)
+}
