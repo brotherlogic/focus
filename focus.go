f1c5d3ee438505975f4a180b09a39ea47b1614ba
Simon Tucker <brotherlogic@gmail.com>
2023-01-13 17:29:41 -0800

    Ignore metrics

--- focus.go
+++ focus.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"time"
 
 	"github.com/brotherlogic/goserver"
 	"golang.org/x/net/context"
@@ -15,7 +11,6 @@ import (
 	github_client "github.com/brotherlogic/githubcard/client"
 
 	pbg "github.com/brotherlogic/goserver/proto"
-	"github.com/brotherlogic/goserver/utils"
 )
 
 // Server main server type
@@ -63,14 +58,6 @@ func (s *Server) GetState() []*pbg.State {
 }
 
 func main() {
-	var quiet = flag.Bool("quiet", false, "Show all output")
-	flag.Parse()
-
-	//Turn off logging
-	if *quiet {
-		log.SetFlags(0)
-		log.SetOutput(ioutil.Discard)
-	}
 	server := Init()
 	server.PrepServer("focus")
 	server.Register = server
@@ -79,10 +66,6 @@ func main() {
 	if err != nil {
 		return
 	}
-
-	ctx, cancel := utils.ManualContext("tasklist-init", time.Minute)
-	server.readConfig(ctx)
-	cancel()
 
 	fmt.Printf("%v", server.Serve())
 }
