16037fbed97da6f4ee68aa65b02ff6dde996a216
Simon Tucker <brotherlogic@gmail.com>
2023-01-14 12:53:12 -0800

    Coverage upping

--- focus_home_test.go
+++ focus_home_test.go
@@ -3,15 +3,21 @@ package main
 import (
 	"context"
 	"testing"
+	"time"
 
+	pbds "github.com/brotherlogic/dstore/proto"
 	pb "github.com/brotherlogic/focus/proto"
 	pbgh "github.com/brotherlogic/githubcard/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 func TestGetHomeTasksSucceed(t *testing.T) {
 	s := InitTestServer()
 	s.foci = []FocusBuilder{s.getHomeTaskFocus}
-	s.ghClient.AddIssue(context.Background(), &pbgh.Issue{Title: "Test", Service: "home"})
+	s.ghClient.AddIssue(context.Background(), &pbgh.Issue{Title: "Test", Service: "home", DateAdded: time.Now().Unix()})
+	s.ghClient.AddIssue(context.Background(), &pbgh.Issue{Title: "Test2", Service: "home", DateAdded: time.Now().Add(time.Hour).Unix()})
 
 	res, err := s.GetFocus(context.Background(), &pb.GetFocusRequest{})
 	if err != nil {
@@ -20,5 +26,44 @@ func TestGetHomeTasksSucceed(t *testing.T) {
 
 	if res.GetFocus().GetType() != pb.Focus_FOCUS_ON_HOME_TASKS {
 		t.Errorf("Bad focus: %v", res)
+	}
+
+	if res.GetFocus().GetDetail() != "Test" {
+		t.Errorf("Bad ordering on home tasks")
+	}
+}
+
+func TestGetHomeTasksFailOnIssuePull(t *testing.T) {
+	s := InitTestServer()
+	s.foci = []FocusBuilder{s.getHomeTaskFocus}
+	s.ghClient.ErrorCode = codes.DataLoss
+
+	_, err := s.GetFocus(context.Background(), &pb.GetFocusRequest{})
+	if err == nil {
+		t.Fatalf("Bad focus pull for home tasks: %v", err)
+	}
+}
+
+func TestGetHomeTasksFailOnNoIssues(t *testing.T) {
+	s := InitTestServer()
+	s.foci = []FocusBuilder{s.getHomeTaskFocus}
+
+	_, err := s.GetFocus(context.Background(), &pb.GetFocusRequest{})
+	if err == nil {
+		t.Fatalf("Bad focus pull for home tasks: %v", err)
+	}
+}
+
+func TestGetHomeTasksFailOnHomeTasksDone(t *testing.T) {
+	s := InitTestServer()
+	s.foci = []FocusBuilder{s.getHomeTaskFocus}
+	s.ghClient.ErrorCode = codes.DataLoss
+	config := &pb.Config{HomeCount: 2}
+	data, _ := proto.Marshal(config)
+	s.dsClient.Write(context.Background(), &pbds.WriteRequest{Key: CONFIG, Value: &anypb.Any{Value: data}})
+
+	_, err := s.GetFocus(context.Background(), &pb.GetFocusRequest{})
+	if err == nil {
+		t.Fatalf("Bad focus pull for home tasks: %v", err)
 	}
 }
