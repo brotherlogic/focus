b0234230a71653f8467da732fd19db2dea14aa05
Simon Tucker <brotherlogic@gmail.com>
2023-01-14 14:41:21 -0800

    Covers load failure

--- focusutils_test.go
+++ focusutils_test.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"testing"
 
 	"github.com/brotherlogic/godiscogs"
+	"google.golang.org/grpc/codes"
 )
 
 func TestEmptyImages(t *testing.T) {
 	res := getImage([]*godiscogs.Image{})
 	if res != "" {
 		t.Errorf("Should have been blank: %v", res)
+	}
+}
+
+func TestBadLoad(t *testing.T) {
+	s := InitTestServer()
+	s.dsClient.ErrorCode = make(map[string]codes.Code)
+	s.dsClient.ErrorCode[CONFIG] = codes.DataLoss
+
+	config, err := s.load(context.Background())
+	if err == nil {
+		t.Errorf("Should have failed: %v", config)
 	}
 }
