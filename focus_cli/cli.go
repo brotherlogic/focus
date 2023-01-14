590aeaf6d1c0f57222f38b4e2fe7bce79464f581
Simon Tucker <brotherlogic@gmail.com>
2023-01-14 08:53:47 -0800

    Adds cli

---
+++ focus_cli/cli.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/brotherlogic/goserver/utils"
+
+	pb "github.com/brotherlogic/focus/proto"
+)
+
+func main() {
+	ctx, cancel := utils.ManualContext("focus-cli", time.Minute)
+	defer cancel()
+
+	conn, err := utils.LFDialServer(ctx, "focus")
+	if err != nil {
+		log.Fatalf("Bad dial: %v", err)
+	}
+
+	client := pb.NewFocusServiceClient(conn)
+	focus, err := client.GetFocus(ctx, &pb.GetFocusRequest{})
+	if err != nil {
+		log.Fatalf("Bad focus: %v", err)
+	}
+
+	fmt.Printf("%v (%v)\n", focus.GetFocus().GetDetail(), focus.GetFocus().GetLink())
+}
