498543072578de9cb7fedc15859dcf9c445a8b7f
Simon Tucker <brotherlogic@gmail.com>
2023-08-14 08:52:35 -0700

    Uhuh

--- focus_deep.go
+++ focus_deep.go
@@ -13,7 +13,7 @@ import (
 )
 
 func (s *Server) getDeepFocus(ctx context.Context, config *pb.Config) (*pb.Focus, error) {
-	if time.Now().Hour() == 19 {
+	if time.Now().Hour() == 19 || time.Now().Hour() < 16 {
 		return nil, fmt.Errorf("not the time for deep focus")
 	}
 
